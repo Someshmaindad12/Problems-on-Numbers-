9a3d9ec43da184d5bf46594e9bfeb0a83d5b6118
Someshwar Maindad <124545422+Someshmaindad12@users.noreply.github.com>
2025-02-01 13:35:55 +0530

    Add files via upload

---
+++ program5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool CheckDivisible(int iNo)
+{
+    if((iNo % 3 == 0 )&&(iNo % 5 == 0))
+    {
+        return true ;
+    }
+    else 
+    {
+        return false ;
+    }
+}
+int main()
+{
+    int iValue = 0 ; 
+    bool bRet = false;
+    printf("Enter Number : \t");
+    scanf("%d",&iValue);
+    bRet = CheckDivisible(iValue);
+    if(bRet == true )
+    {
+        printf("%d is divisible by 3 and 5",iValue);
+    }
+    else 
+    {
+        printf("%d is NOT divisible by 3 and 5",iValue);
+    }
+    return 0;
+}
